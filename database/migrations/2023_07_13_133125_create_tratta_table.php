f984cdbf4804223e3619a88e4d74f0c7de526b6d
Menta <simcictilen.gmail.com>
2023-07-13 16:04:26 +0200

    first commit

---
+++ database/migrations/2023_07_13_133125_create_tratta_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tratta', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('azienda', 50);
+            $table->string('stazione_di_partenza', 64);
+            $table->string('stazione_di_arrivo', 64);
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->string('codice_treno', 32) -> unique();
+            $table->integer('numero_carrozze');
+            $table->boolean('in_orario') -> default(true);
+            $table->boolean('cancellato') -> default(false);
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tratta');
+    }
+};
